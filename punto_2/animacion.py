94ec8a39f0252cdbcbbcd6b5db032585f3cacd86
Juan Pablo Gonzalez Aguilera <UANDES\jp.gonzalez15@fisicaez12203l.ad.uniandes.edu.co>
2018-05-09 18:30:59 -0500

    a

--- punto_2/animacion.py
+++ punto_2/animacion.py
@@ -3,6 +3,23 @@ import numpy as np
 from matplotlib.animation import FuncAnimation
 
 uxt=np.loadtxt('datos.txt')
-plt.plot(uxt[:,0],uxt[:,1])
+x=np.linspace(0,100,101)
+# First set up the figure, the axis, and the plot element we want to animate
+fig = plt.figure()
+ax = plt.axes(xlim=(0, 100), ylim=(-1, 1))
+line, = ax.plot([], [], lw=2)
+
+# initialization function: plot the background of each frame
+def init():
+    line.set_data([], [])
+    return line,
+
+# animation function.  This is called sequentially
+def animate(i):
+    x = np.linspace(0, 2, 1000)
+    y = np.sin(2 * np.pi * (x - 0.01 * i))
+    line.set_data(x, y)
+    return line,
+anim = animation.FuncAnimation(fig, animate, init_func=init,frames=20, interval=20, blit=True)
 plt.show()
 #plt.savefig('cuerda.gif')
--- punto_2/cuerda.cpp
+++ punto_2/cuerda.cpp
@@ -8,20 +8,14 @@ int main()
 	double c = 2.0;
 	double L = 100;
 	double T = 200;
-	double dx = 0.01;
-	double dt = 0.001;
+	double dx = 1;
+	double dt = 0.1;
 	int Nx = L/dx +1;
 	int Nt = T/dt +1;
 	int Nsnaps = 20;
 	
-	double **uxt_print = new double *[Nsnaps];//arreglo que se va a imprimir
-	
 	int i;//recorre tiempo
 	int j;//recore x
-	for(i=0;i<Nsnaps;i++)
-	{
-		uxt_print[i]= new double[Nx];
-	}
 	//arreglos iterativos
 	double *uxtnew = new double[Nx];
 	double *uxtcenter = new double[Nx];
@@ -41,16 +35,27 @@ int main()
 			uxtold[j]=5.0-5.0*x/L;
 			uxtcenter[j]=5.0-5.0*x/L;
 		}
-		//cout<<x<<" "<<uxtold[j]<<endl;
+		cout<<uxtold[j]<<" ";
 	}
+	cout<<endl;
 	uxtnew[0]=0;
 	uxtnew[Nx-1]=0;
 	//recursion ec. dif. 
+	int snap = 0;
 	for(i=1;i<Nt;i++)
 	{
+		int t=i*dt;
+		if(t!=0 and t % 10 == 0)
+		{
+			for(j=0;j<Nx;j++)
+			{
+				cout<<uxtold[j]<<" ";
+			}
+			cout<<endl;
+		}
 		for(j=1;j<Nx-1;j++)
 		{
-			utxnew[j]=c*c*dt*dt*(uxtcenter[i+1]+uxtcenter[i-1]-2.0*uxtcenter[i])/(dx*dx)+2.0*uxtcenter[i]-uxtold[i];
+			uxtnew[j]=c*c*dt*dt*(uxtcenter[j+1]+uxtcenter[j-1]-2.0*uxtcenter[j])/(dx*dx)+2.0*uxtcenter[j]-uxtold[j];
 		}
 		for(j=0;j<Nx;j++)
 		{
@@ -58,7 +63,6 @@ int main()
 			uxtcenter[j]=uxtnew[j];
 		}
 	}
-	
-	
+
 	return 0;
 }
