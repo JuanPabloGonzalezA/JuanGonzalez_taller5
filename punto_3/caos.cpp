cd6510edfe0e413d31626f680ef49f9f16cd6b2a
Juan Pablo Gonzalez Aguilera <UANDES\jp.gonzalez15@fisicaez12205l.ad.uniandes.edu.co>
2018-05-11 10:55:22 -0500

    p3

--- punto_3/caos.cpp
+++ punto_3/caos.cpp
@@ -14,9 +14,29 @@ int main()
 	double dt=0.006;
 	double T=3000;
 	int Nt = T/dt;
+	double d
 	double q1=1.0/(2.0*sqrt(2.0));
+	double p1=0.0;
+	double q2=-1.0/(2.0*sqrt(2.0));
+	double p2=0.0;
+
+	
 	cout<<EPSILON<<" "<<12<<endl;
 	cout<<EPSILON<<" "<<12<<endl;
+
+	//inicio rungekuta
+	
+	int i;
+	for(i=0;i<Nt;i++)
+	{
+		k1q1=dt*dq1(p1);
+		k1q2=dt*dq1(p1);
+		k1q1=dt*dq1(p1);
+		k1q1=dt*dq1(p1);
+
+		
+	}
+
 	return 0;
 }
 double dq1(double p1)
@@ -29,7 +49,7 @@ double dq2(double p2)
 }
 double dp1(double q1)
 {
-	return -1.0*(q1)/(pow((4.0*q1*q1 + EPSILON*EPSILON),1.5));
+	return -2.0*(q1)/(pow((4.0*q1*q1 + EPSILON*EPSILON),1.5));
 }
 double dp2(double q1, double q2)
 {
